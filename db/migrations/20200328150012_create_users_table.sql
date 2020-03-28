f020d6d2b7973e2cae73a81a51a66f1bd49f79e9
Jorge L <jorge.alfmur@gmail.com>
2020-03-28 15:32:42 -0500

    add migration to create users table

---
+++ db/migrations/20200328150012_create_users_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+CREATE TABLE users (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  firstName VARCHAR(100) NOT NULL,
+  lastName VARCHAR(255) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  createdAt DATETIME,
+  updatedAt DATETIME
+);
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE users;
