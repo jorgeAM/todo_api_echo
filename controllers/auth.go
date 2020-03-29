7bbd5f8cf92165ba3c6d211da830d732f3404cf9
Jorge L <jorge.alfmur@gmail.com>
2020-03-28 23:17:52 -0500

    login but only return a user in the future it'd return a jwt

--- controllers/auth.go
+++ controllers/auth.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jorgeAM/echo-api/models"
 	"github.com/jorgeAM/echo-api/utils"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // SignUp creates a new user
@@ -46,5 +47,25 @@ func SignUp(c echo.Context) error {
 
 // Login returns a jwt
 func Login(c echo.Context) error {
-	return c.String(http.StatusOK, "Login")
+	db := connection.Conn()
+	defer db.Close()
+	u := new(models.User)
+
+	if err := c.Bind(&u); err != nil {
+		return err
+	}
+
+	pwd := u.Password
+
+	db.Where("email = ?", u.Email).First(&u)
+
+	if u.ID <= 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, "account does not exist")
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(pwd)); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "incorrect password")
+	}
+
+	return c.JSON(http.StatusOK, &u)
 }
