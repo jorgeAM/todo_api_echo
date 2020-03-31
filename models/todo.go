6c1d5a868516174252d898731688cbdcbc2da1ed
Jorge L <jorge.alfmur@gmail.com>
2020-03-30 22:37:47 -0500

    hide timestamp

--- models/todo.go
+++ models/todo.go
@@ -8,6 +8,6 @@ type Todo struct {
 	Title       string    `json:"title" gorm:"not null"`
 	Description string    `json:"description" gorm:"not null"`
 	UserID      *uint     `json:"-" gorm:"column:userId"`
-	CreatedAt   time.Time `json:"createdAt" gorm:"column:createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt" gorm:"column:updatedAt"`
+	CreatedAt   time.Time `json:"-" gorm:"column:createdAt"`
+	UpdatedAt   time.Time `json:"-" gorm:"column:updatedAt"`
 }
